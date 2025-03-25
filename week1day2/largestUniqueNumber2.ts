311cedc8df7a708fd8df0eed0af37db591cf97ec
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-03-26 01:14:28 +0900

    Update condition

--- week1day2/largestUniqueNumber2.ts
+++ week1day2/largestUniqueNumber2.ts
@@ -1,5 +1,8 @@
 export function largestUniqueNumberWithoutAdditionalMemory(nums: number[]): number {
   nums.sort((a, b) => a - b);
+  if (nums.length === 1) {
+    return nums[0];
+  }
   let i = nums.length - 1;
   while (i >= 0) {
     // seen a duplicate
@@ -21,6 +24,9 @@ export function largestUniqueNumberWithoutAdditionalMemory(nums: number[]): numb
 
 export function largestUniqueNumberWithoutAdditionalMemory2(nums: number[]): number {
   nums.sort((a, b) => b - a);
+  if (nums.length === 1) {
+    return nums[0];
+  }
   for (let i = 0; i < nums.length; i++) {
     if ((i === 0 || nums[i] !== nums[i - 1]) && 
           (i === nums.length - 1 || nums[i] !== nums[i + 1])) {
